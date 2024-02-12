30c99dd9ac8a5679d5e6ca4359acd4313fa1eafe
LZTD1 <danil227pavlov@gmail.com>
2024-02-12 20:54:19 +0300

    Мокирование синглтона + мелкий рефакторинг

---
+++ bot/src/test/java/hw1/processors/TestStartHandler.java
@@ -0,0 +1,30 @@
+package hw1.processors;
+
+import edu.java.database.SimpleDatabase;
+import edu.java.processor.processors.StartHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import static edu.java.database.SimpleDatabase.getInstance;
+import static edu.java.processor.Constants.START_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestStartHandler {
+
+    private static final SimpleDatabase db = getInstance();
+
+    @BeforeAll
+    static void dropDb() {
+        db.dropAll();
+    }
+
+    @Test
+    public void testStart() {
+        var mock = Mockito.mock(Update.class);
+
+        var method = new StartHandler();
+
+        assertThat(method.handle(mock)).isEqualTo(START_MESSAGE);
+    }
+}
---
+++ bot/src/test/java/hw1/processors/TestUntrackHandler.java
@@ -0,0 +1,55 @@
+package hw1.processors;
+
+import edu.java.database.SimpleDatabase;
+import edu.java.processor.processors.UntrackHandler;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import static edu.java.database.SimpleDatabase.getInstance;
+import static edu.java.processor.Constants.FAIL_UNTRACK_MESSAGE;
+import static edu.java.processor.Constants.SUCCESSFUL_UNTRACK_MESSAGE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestUntrackHandler {
+
+    private static final SimpleDatabase db = getInstance();
+
+    @BeforeEach
+    void dropDb() {
+        db.dropAll();
+    }
+
+    @Test
+    public void testUnTrackFail() {
+        var mock = Mockito.mock(Update.class);
+        var mockedMessage = Mockito.mock(Message.class);
+        Mockito.when(mock.getMessage()).thenReturn(mockedMessage);
+        Mockito.when(mock.getMessage().getChatId()).thenReturn(1L);
+        Mockito.when(mock.getMessage().getText()).thenReturn(
+            new UntrackHandler().getName() + " test test"
+        );
+
+        var method = new UntrackHandler();
+        assertThat(method.handle(mock)).isEqualTo(FAIL_UNTRACK_MESSAGE);
+        assertThat(db.getUserLinksById(1L)).isEmpty();
+    }
+
+    @Test
+    public void testUnTrackSucessfull() {
+        var mock = Mockito.mock(Update.class);
+        var mockedMessage = Mockito.mock(Message.class);
+        Mockito.when(mock.getMessage()).thenReturn(mockedMessage);
+        Mockito.when(mock.getMessage().getChatId()).thenReturn(1L);
+        Mockito.when(mock.getMessage().getText()).thenReturn(
+            new UntrackHandler().getName() + " test"
+        );
+
+        var method = new UntrackHandler();
+        assertThat(method.handle(mock)).isEqualTo(SUCCESSFUL_UNTRACK_MESSAGE);
+        assertThat(db.getUserLinksById(1L)).isEmpty();
+
+    }
+}
