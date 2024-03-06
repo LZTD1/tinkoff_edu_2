7ea914cde55fa276d90a0bff3d4078165110e2b4
LZTD1 <danil227pavlov@gmail.com>
2024-03-06 16:53:05 +0300

    Рефакторинг миграций в SQL формат

---
+++ migrations/liquibase/changelog/changeset/createUserLinksTable.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users_links
+(
+    userId BIGINT NOT NULL,
+    linkId BIGINT NOT NULL,
+    PRIMARY KEY (userId, linkId)
+);
+
+ALTER TABLE users_links
+    ADD CONSTRAINT fk_relUser_userId
+        FOREIGN KEY (userId)
+            REFERENCES users(id);
+
+ALTER TABLE users_links
+    ADD CONSTRAINT fk_relLink_linkId
+        FOREIGN KEY (linkId)
+            REFERENCES links(id);
