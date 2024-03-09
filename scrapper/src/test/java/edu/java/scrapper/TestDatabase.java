567ee61adc57673ffc1175f0b56a6751c14b57fa
Danil <danil227pavlov@gmail.com>
2024-03-09 17:53:12 +0300

    Рефакторинг мастер файла

---
+++ scrapper/src/test/java/edu/java/scrapper/TestDatabase.java
@@ -0,0 +1,51 @@
+package edu.java.scrapper;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestDatabase extends IntegrationTest {
+
+    private static JdbcTemplate jdbc;
+
+    @BeforeAll
+    static void setUp() {
+        jdbc = new JdbcTemplate(
+            DataSourceBuilder
+                .create()
+                .url(POSTGRES.getJdbcUrl())
+                .username(POSTGRES.getUsername())
+                .password(POSTGRES.getPassword())
+                .build()
+        );
+    }
+
+    @Test
+    void testPostgresIsRunning() {
+        assertTrue(POSTGRES.isRunning());
+    }
+
+    @Test
+    void testCreateUserEntity() {
+        String sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = ?";
+        Integer tableExists = jdbc.queryForObject(sql, Integer.class, "users");
+        assertThat(tableExists.intValue()).isEqualTo(1);
+    }
+
+    @Test
+    void testCreateLinkEntity() {
+        String sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = ?";
+        Integer tableExists = jdbc.queryForObject(sql, Integer.class, "links");
+        assertThat(tableExists.intValue()).isEqualTo(1);
+    }
+
+    @Test
+    void testCreateUserLinkTable() {
+        String sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_name = ?";
+        Integer tableExists = jdbc.queryForObject(sql, Integer.class, "users_links");
+        assertThat(tableExists.intValue()).isEqualTo(1);
+    }
+}
